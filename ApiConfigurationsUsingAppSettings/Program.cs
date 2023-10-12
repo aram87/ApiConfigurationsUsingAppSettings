1b06f23e681db4eb6178c86ee691c1f4ea6baf30
Aram Tchekrekjian <AramT@Aramex.com>
2023-10-12 11:34:23 +0300

    Update related to production environment

--- ApiConfigurationsUsingAppSettings/Program.cs
+++ ApiConfigurationsUsingAppSettings/Program.cs
@@ -20,11 +20,11 @@ builder.Services.Configure<ApiSettingsModel>(section);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
